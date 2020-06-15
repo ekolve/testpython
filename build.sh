154a5fc62e0cdf1118e08c93531050b62f1ebe47
Eric Kolve <erick@allenai.org>
2020-06-15 12:54:38 -0700

    adding twine creds

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+pip3 install wheel twine
 
 if [[ $(git rev-parse --is-shallow-repository) == 'true' ]]; then
     git fetch --unshallow
     git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
     git fetch origin
 fi;
-env
+
+python setup.py sdist bdist_wheel --universal
+twine upload --repository testpypi -u __token__ dist/*
